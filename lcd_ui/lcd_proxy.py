a7d0a1315af72a1f916bc7229a3a99ed9e9d7cad
Trent Novelly <trentnov@gmail.com>
2021-01-23 13:01:33 -0500

    initial write functions

--- lcd_ui/lcd_proxy.py
+++ lcd_ui/lcd_proxy.py
@@ -4,11 +4,13 @@ class LCDProxy(object):
         self.rows = rows
         self.lcd = [bytearray(init_char*self.chars,encoding='utf-8') for row in range(self.rows)]
 
+    def write(self,char,pos):
+        self.lcd[pos[0]][pos[1]] = ord(char)
+    
     def write_word(self,word,start_pos):
-        pass
-
-    def write(self,pos,char):
-        self.lcd[pos[0]][pos[1]] = char
+        for char in word:
+            self.write(char,start_pos)
+            start_pos[1] = start_pos[1]+1
 
     def update(self):
         #TODO: write to lcd
--- tests/test_lcd_proxy.py
+++ tests/test_lcd_proxy.py
@@ -6,11 +6,27 @@ class TestLCDProxy(unittest.TestCase):
         p = LCDProxy()
         self.assertEqual(p.lcd, [bytearray(' '*16,encoding='utf-8'),bytearray(' '*16,encoding='utf-8')])
 
-
     def test_str_repr(self):
         p = LCDProxy(chars=5, rows=1, init_char='A')
         intended_output ='A A A A A' 
         self.assertEqual(str(p),intended_output)
+
+    def test_write(self):
+        p = LCDProxy(chars=5, rows=1)
+        intended_output = 'A B C D E'
+        p.write('A',[0,0])
+        p.write('B',[0,1])
+        p.write('C',[0,2])
+        p.write('D',[0,3])
+        p.write('E',[0,4])
+        self.assertEqual(str(p),intended_output)
+
+    def test_write_word(self):
+        p = LCDProxy(chars=5,rows=1)
+        intended_output = 'A B C D E'
+        p.write_word('ABCDE',[0,0])
+        self.assertEqual(str(p),intended_output)
+
 
 if __name__ == '__main__':
     unittest.main()
