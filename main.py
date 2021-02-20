b9282ee41f7240e7e2295ef519eec1910b69f0d5
Trent Novelly <trentnov@gmail.com>
2021-02-20 23:05:40 +0000

    input buttons set up

--- main.py
+++ main.py
@@ -14,8 +14,47 @@ import argparse
 import threading
 from lcd_ui import UI, LCDProxy
 from temp_controller_ui import build_ui
+import time
 
 def input_thread(event_queue):
+    #button setup
+    def setup_buttons():
+        #setup down
+        GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(20, GPIO.RISING, callback=down_button_callback)
+        
+        GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(21, GPIO.RISING, callback=up_button_callback)
+
+        GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(18, GPIO.RISING, callback=back_button_callback)
+
+        GPIO.setup(23,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=select_button_callback)
+    
+    #input handler callbacks
+    def down_button_callback(channel):
+        time.sleep(0.04)
+        if GPIO.input(channel):
+            event_queue.put({'type':'input','val':'down'}) 
+    
+    def up_button_callback(channel):
+        time.sleep(0.04)
+        if GPIO.input(channel):
+            event_queue.put({'type':'input','val':'up'})
+
+    def back_button_callback(channel):
+        time.sleep(0.04)
+        if GPIO.input(channel):
+            event_queue.put({'type':'input','val':'back'})
+
+    def select_button_callback(channel):
+        time.sleep(0.04)
+        if GPIO.input(channel):
+            event_queue.put({'type':'input','val':'select'})
+
+
+    setup_buttons()
     while True:
         i = input()
         event_queue.put({'type':'input','val':i})
@@ -45,4 +84,4 @@ if __name__ == '__main__':
     input_t.start()
 
     ui_t.join()
-
+    GPIO.cleanup()
