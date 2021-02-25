85df8d955ad158b4ec1df7844edc6e7012b62a75
Trent Novelly <trentnov@gmail.com>
2021-02-25 04:40:08 +0000

    menu refactored as list input

--- lcd_ui/ui_elements/input_elements.py
+++ lcd_ui/ui_elements/input_elements.py
@@ -1,4 +1,9 @@
-from . import UI_Element
+import time
+import random
+import threading
+
+from . import UI_Element, InvalidDirection
+from .content import Content, DynamicContent
 
 class DialInput(UI_Element):
     def __init__(self, label, init_value):
@@ -31,3 +36,62 @@ class SelectTemp(DialInput):
             str_val = str(self.value)
             f.write(str_val+'0'*max(0,5-len(str_val)))
         self.set = True
+
+
+
+
+class ListInput(UI_Element):
+    def __init__(self, options, num_lines=2):
+        self._options = options
+        self._num_lines = num_lines
+        self._select_line = 0
+        self._display_start = 0
+        self._display_threads = []
+        self.is_displayed = False
+
+    def scroll_display(self,dir):
+        if dir == 'up':
+            if self._display_start > 0:
+                self._display_start = (self._display_start - 1)
+        elif dir == 'down':
+            if self._display_start < len(self._options)-self._num_lines:
+                self._display_start = (self._display_start + 1)
+        else:
+            raise InvalidDirection
+
+    def scroll(self,dir):
+        if dir == 'up':
+            if self._select_line > 0:
+                self._select_line = (self._select_line - 1)
+            else:
+                self.scroll_display('up')
+        elif dir == 'down':
+            if self._select_line < self._num_lines-1:
+                self._select_line = (self._select_line + 1)
+            else:
+                self.scroll_display('down')
+        else:
+            raise InvalidDirection
+
+    def cycle(self):
+        pass
+
+    def select(self):
+        pass
+
+    def get_display(self):
+        lines = self._options[self._display_start:self._display_start+self._num_lines]
+        lines = [">" + str(line[0]) if i==self._select_line else " " + str(line[0]) for i,line in enumerate(lines)]
+        return lines
+
+    def start(self,event_queue):
+        self.is_displayed = True
+        for entry in self._options:
+            if isinstance(entry[0],DynamicContent):
+                t = threading.Thread(target=entry[0].run,args=(event_queue,),daemon=True)
+                t.start()
+                self._display_threads.append(t)
+    
+    def stop(self):
+        self.is_displayed = False
+
--- lcd_ui/ui_elements/menu.py
+++ lcd_ui/ui_elements/menu.py
@@ -1,79 +1,10 @@
-import time
-import random
-import threading
+from .input_elements import ListInput
 
-from . import UI_Element, InvalidDirection
-from .content import Content, DynamicContent
-
-class Menu(UI_Element):
+class Menu(ListInput):
     def __init__(self,entries,num_lines=2):
-        self._entries = entries
-        for entry in self._entries:
+        super().__init__(entries,num_lines)
+        for entry in self._options:
             entry[0].set_parent(self)
 
-        self._num_lines = num_lines
-        self._select_line = 0
-        self._display_start = 0
-        self._display_threads = []
-        self.is_displayed = False
-
-    def scroll_display(self,dir):
-        if dir == 'up':
-            if self._display_start > 0:
-                self._display_start = (self._display_start - 1)
-        elif dir == 'down':
-            if self._display_start < len(self._entries)-self._num_lines:
-                self._display_start = (self._display_start + 1)
-        else:
-            raise InvalidDirection
-
-    def scroll(self,dir):
-        if dir == 'up':
-            if self._select_line > 0:
-                self._select_line = (self._select_line - 1)
-            else:
-                self.scroll_display('up')
-        elif dir == 'down':
-            if self._select_line < self._num_lines-1:
-                self._select_line = (self._select_line + 1)
-            else:
-                self.scroll_display('down')
-        else:
-            raise InvalidDirection
-
-    def cycle(self):
-        pass
-
     def select(self):
-        return self._entries[self._display_start+self._select_line][1]
-
-    def get_display(self):
-        lines = self._entries[self._display_start:self._display_start+self._num_lines]
-        lines = [">" + str(line[0]) if i==self._select_line else " " + str(line[0]) for i,line in enumerate(lines)]
-        return lines
-
-    def start(self,event_queue):
-        self.is_displayed = True
-        for entry in self._entries:
-            if isinstance(entry[0],DynamicContent):
-                t = threading.Thread(target=entry[0].run,args=(event_queue,),daemon=True)
-                t.start()
-                self._display_threads.append(t)
-    
-    def stop(self):
-        self.is_displayed = False
-
-# class TextInput(UI_Element):
-#     def __init__(self,max_chars=16):
-#         self._current_input = []
-#         self._current_selected = 'A'
-#         self._input_options = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-#         self._input_elem_start = 0
-#         self._select_item = 0
-        
-#     def select(self):
-#         self._current_input.append(self._input_options)
-
-#     def get_display(self):
-#         return [''.join(self._current_input)+,''.join(self._input_options[self._input_elem_start:self._input_elem_start+16]])]
-
+        return self._options[self._display_start+self._select_line][1]
